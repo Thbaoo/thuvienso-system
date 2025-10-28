9edcd87734a6f69043a181f638f80334371b91f4
thbaoo <thbaoo109@gmail.com>
2025-10-28 14:49:54 +0000

    update

--- static/js/search.js
+++ static/js/search.js
@@ -1,28 +1,28 @@
-  const input = document.getElementById("searchInput");
-  const resultsContainer = document.getElementById("resultsContainer");
-  let books = [];
+const input = document.getElementById("searchInput");
+const resultsContainer = document.getElementById("resultsContainer");
+let books = [];
 
-  // Tải dữ liệu từ file JSON
-  fetch("static/data/books.json")
-    .then(res => res.json())
-    .then(data => {
-      books = data;
-    });
-
-  // Lắng nghe sự kiện nhập
-  input.addEventListener("input", () => {
-    const keyword = input.value.toLowerCase();
-    const filtered = books.filter(book =>
-      book.title.toLowerCase().includes(keyword)
-    );
-
-    resultsContainer.innerHTML = filtered.length
-      ? filtered.map(b => `
-          <div class="book-card">
-            <img src="${b.img}" alt="${b.title}">
-            <h3>${b.title}</h3>
-            <p>Năm xuất bản: ${b.year}</p>
-          </div>
-        `).join("")
-      : "<p>Không tìm thấy sách nào phù hợp.</p>";
+// Tải dữ liệu từ file JSON
+fetch("static/data/books.json")
+  .then(res => res.json())
+  .then(data => {
+    books = data;
   });
+
+// Lắng nghe sự kiện nhập
+input.addEventListener("input", () => {
+  const keyword = input.value.toLowerCase();
+  const filtered = books.filter(book =>
+    book.title.toLowerCase().includes(keyword)
+  );
+
+  resultsContainer.innerHTML = filtered.length
+    ? filtered.map(b => `
+        <div class="book-card" onclick="window.location.href='${b.link}'">
+          <img src="${b.img}" alt="${b.title}">
+          <h3>${b.title}</h3>
+          <p>Năm xuất bản: ${b.year}</p>
+        </div>
+      `).join("")
+    : "<p>Không tìm thấy sách nào phù hợp.</p>";
+});
